23f2ba7103077fcb7403dd254fcd9a5c479c96b4
Max <mdiazn@uni.pe>
2025-04-23 21:07:49 -0500

    Paginación simple.

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,22 @@ import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany();
+  const { searchParams } = new URL(request.url);
+
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "Tiene que ser un numero" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
 
   return NextResponse.json(todos, { status: 200 });
 }
