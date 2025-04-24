275c01c30838fd81600cb4ef51e3004483da8445
Max <mdiazn@uni.pe>
2025-04-24 10:44:07 -0500

    Actualizar entradas - PUT.

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -17,3 +17,21 @@ export async function GET(request: Request, { params }: Segments) {
 
   return NextResponse.json(todo, { status: 200 });
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+  const id = params.id;
+  const todo = await prisma.todo.findUnique({ where: { id } });
+
+  if (!todo) {
+    return NextResponse.json({ message: "Todo not found" }, { status: 404 });
+  }
+
+  const body = await request.json();
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: { ...body },
+  });
+
+  return NextResponse.json(updatedTodo, { status: 200 });
+}
