9e88d29dd7ebd3e365918af61367bf24d2092ba4
Max <mdiazn@uni.pe>
2025-04-25 10:44:42 -0500

    Listar las entradas.

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,25 @@
-export default function RestTodosPage() {
+import prisma from "@/lib/prisma";
+
+export const metadata = {
+  title: "Listado de pendientes",
+  description: "SEO title",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+
+  // useEffect(() => {
+  //   fetch("/api/todos")
+  //     .then((res) => res.json())
+  //     .then(console.log);
+  // }, []);
+
   return (
     <div>
       <h1>RestTodos Page</h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.description}</div>
+      ))}
     </div>
   );
 }
