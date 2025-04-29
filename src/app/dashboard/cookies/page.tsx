9b6a6ff6aad4c3fc6946a09be42f25d4a8e0083a
Max <mdiazn@uni.pe>
2025-04-29 11:02:44 -0500

    Cookies - Server Side.

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,16 +1,20 @@
 import { TabBar } from "@/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies",
   description: "SEO title",
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const cookieTab = cookieStore.get("selectedTab")?.value ?? "1";
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={+cookieTab} />
       </div>
     </div>
   );
