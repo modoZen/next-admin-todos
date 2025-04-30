c0760778d3b0941b8faf927577ea007fb35e8baa
Max <mdiazn@uni.pe>
2025-04-30 13:01:47 -0500

    Información del usuario - Server Side.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 import { WidgetItem } from "@/components/WidgetItem";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+    <div className="grid gap-6 md:grid-cols-1 lg:grid-cols-2">
+      <WidgetItem title="Usuario contecado server side">
+        <div className="flex flex-col">
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+          <span>{session.user?.email}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }
