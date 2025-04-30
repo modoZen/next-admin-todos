7636609f75dc2506af0a2fdc93568b7b56d32681
Max <mdiazn@uni.pe>
2025-04-30 12:50:36 -0500

    GithubProvider.

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,6 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
+  providers: [GitHub],
 });
