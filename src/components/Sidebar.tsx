71cbadd0d9edd22a8d33b6d4e64a098eb7b52552
Max <mdiazn@uni.pe>
2025-04-30 21:29:36 -0500

    Logout y SignIn.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-icons/io5";
 import { SidebarItem } from "./SidebarItem";
 import { auth } from "@/auth";
+import { LogoutButton } from "./LogoutButton";
 
 const sidebarItems = [
   {
@@ -92,10 +93,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
+        <LogoutButton />
       </div>
     </aside>
   );
